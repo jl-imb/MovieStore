b8779b2acab44b434941af4d8e86a9b132ab7e43
MayssamM11 <mmazlout3@gatech.edu>
2025-01-28 18:08:37 -0500

    added home/templates/home/index.html

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.shortcuts import render
 
 urlpatterns = [
 path('', views.index, name='home.index'),
 ]
+
+def index(request):
+    return render(request, 'home/index.html')
