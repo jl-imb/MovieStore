f0b9d700e31d406602c54916bd959e8df39d9e82
jl-imb <146906702+jl-imb@users.noreply.github.com>
2025-01-31 20:10:25 -0500

    update settings

--- MovieStore/settings.py
+++ MovieStore/settings.py
@@ -55,8 +55,8 @@ ROOT_URLCONF = 'MovieStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR,
+                              'MovieStore/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
