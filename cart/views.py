8c9452b04454369dc7cf4c7b4b96c9d521a986f3
MayssamM11 <mmazlout3@gatech.edu>
2025-02-02 20:42:50 -0500

    added cart tab that displays total amount in cart

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
 from movies.models import Movie
-# from .utils import calculate_cart_total
+from .utils import calculate_cart_total
 
 def index(request):
     cart_total = 0
@@ -10,7 +10,7 @@ def index(request):
     movie_ids = list(cart.keys())
     if (movie_ids != []):
         movies_in_cart = Movie.objects.filter(id__in=movie_ids)
-        # cart_total = calculate_cart_total(cart, movies_in_cart)
+        cart_total = calculate_cart_total(cart, movies_in_cart)
     template_data = {}
     template_data['title'] = 'Cart'
     template_data['movies_in_cart'] = movies_in_cart
@@ -23,4 +23,4 @@ def add(request, id):
     cart = request.session.get('cart', {})
     cart[id] = request.POST['quantity']
     request.session['cart'] = cart
-    return redirect('home.index')
+    return redirect('cart.index')
