8aab927198836e1e6a15ef4c4b4f227626bfb447
jl-imb <146906702+jl-imb@users.noreply.github.com>
2025-02-03 20:15:32 -0500

    import review in admin.py

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Review
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name']
