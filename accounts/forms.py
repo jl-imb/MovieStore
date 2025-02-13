c13173d5c842760ee2a06963a9523162c7cf9e8e
MayssamM11 <mmazlout3@gatech.edu>
2025-02-13 13:02:08 -0500

    reset password i really hope this works

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.utils import ErrorList
+from django import forms
+from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
 
 class CustomErrorList(ErrorList):
@@ -11,8 +13,20 @@ class CustomErrorList(ErrorList):
         ]))
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text="Required. Enter a valid email address.")
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for fieldname in ['username', 'password1', 'password2']:
+        for fieldname in ['username', 'email', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
             self.fields[fieldname].widget.attrs.update({'class': 'form-control'})
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already associated with an existing account.")
+        return email
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,10 @@ from .forms import CustomUserCreationForm, CustomErrorList
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 #from django.contrib.auth.models import User
 
 @login_required
@@ -41,12 +45,12 @@ def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
     if request.method == 'GET':
-        template_data['form'] = UserCreationForm()
+        # template_data['form'] = UserCreationForm()
         template_data['form'] = CustomUserCreationForm()
         return render(request, 'accounts/signup.html',
             {'template_data': template_data})
     elif request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        # form = UserCreationForm(request.POST)
         form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
@@ -56,4 +60,13 @@ def signup(request):
             template_data['form'] = form
             return render(request, 'accounts/signup.html',
                           {'template_data': template_data})
-# Create your views here.
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'accounts/password_reset.html'
+    email_template_name = 'accounts/password_reset_email.html'
+    subject_template_name = 'accounts/password_reset_subject.txt'
+    # success_message = "We've emailed you instructions for setting your password. " \
+    #                   "If an account exists with the email you entered, you should receive them shortly." \
+    #                   " If you don't receive an email within a few minutes, " \
+    #                   "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('home.index')
